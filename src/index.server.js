89831752aa7b1edb7a0cb17c5ce51d2fca3b7f35
sabari <sabari>
2022-12-09 14:32:28 +0530

    today changes

--- src/index.server.js
+++ src/index.server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 const env = require("dotenv");
-
-
+const cors = require("cors");
 env.config();
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        message:"welcome our site"
+    })
+    
+})
 
 app.listen(`${process.env.PORT}`,()=>{
     console.log(`server start our port ${process.env.PORT}`);
