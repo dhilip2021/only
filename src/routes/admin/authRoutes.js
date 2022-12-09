c3cac81f90b2636ed9be1e3f7a2fac940eb61271
sabari <sabari>
2022-12-09 18:07:25 +0530

    changes going

--- src/routes/admin/authRoutes.js
+++ src/routes/admin/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { signup, signin, signout } = require("../../controller/admin/authController");
+const { validateSignupRequest, isRequestValidated, validateSigninRequest } = require("../../validators/authValidators");
 
 const router = express.Router();
 
-router.post("/admin/signup",signup);
-router.post("/admin/signin",signin);
+router.post("/admin/signup",validateSignupRequest, isRequestValidated,signup);
+router.post("/admin/signin",validateSigninRequest, isRequestValidated,signin);
 router.post("/admin/signout",signout);
 
 module.exports = router;
